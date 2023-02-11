3954a6dd35e75d00c063af811baa1a9cb5d5b18e
Gayle Hoefer <gaylehoefer@gmail.com>
2023-02-11 13:29:33 -0700

    Github link is working

--- src/Components/Projects/project.jsx
+++ src/Components/Projects/project.jsx
@@ -1,13 +1,22 @@
 import './project.css'
 import { AiFillGithub } from 'react-icons/ai';
 
-export default function Project({projectInfo}) {
+export default function Project({ projectInfo }) {
     return (
-        <a className="content-card col-11 col-lg-5 my-4" id={projectInfo.id} href={projectInfo.href} target="_blank">
-            <div></div>
-            <p>{projectInfo.projectInfo}</p>
-            <h4>{projectInfo.projectTitle} <AiFillGithub/></h4>
-            <p>{projectInfo.projectGithub}</p>
-        </a>
+        <>
+            <a 
+            className="content-card col-11 col-lg-5 my-4" 
+            id={projectInfo.id}
+            href={projectInfo.href} 
+            target="_blank"
+            >
+                <div></div>
+                <p>{projectInfo.projectInfo}</p>
+                <h4>{projectInfo.projectTitle}
+                    <a href={projectInfo.projectGithub} target="_blank"><AiFillGithub />
+                    </a>
+                </h4>
+            </a>
+        </>
     )
 }
