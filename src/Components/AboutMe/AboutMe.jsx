88b35be545c92e0cf03d47e99bf5f80ca8adcca4
Gayle Hoefer <gaylehoefer@gmail.com>
2023-10-12 19:59:49 -0700

    Adding hcdl (#4)
    * add HCDL picture
    
    * added more to the home page for the intro
    
    * Removed extra text

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -4,10 +4,10 @@ import ProfilePic from '../../images/profile.jpg'
 export default function AboutMe() {
     return (
         <div id="about-me-wrapper">
-            <img src={ProfilePic} alt="Picture of myself"/>
+            <img src={ProfilePic} alt="Female with long brown hair in a white dress witt black strips looking very professional" />
             <section id="about-me">
                 <h3>About Me</h3>
-                <p>Hello! My name is Gayle Hoefer. I am a full-stack web developer from Phoenix, Arizona. A few things about me: I am a problem solver, collaborative, and always motivated to learn. I love playing my eleven instruments. Thank you for visiting my page!</p>
+                <p>I'm Gayle Hoefer, a full stack software engineer with a passion for creating projects through that help people. Throughout my career, I've had the privilege of working on projects that have allowed me to blend creativity with innovation, and my portfolio is a testament to the dedication I have for my craft.</p>
             </section>
         </div>
     )
