9533b8cbc6ee4cd78105e0a47a61d052e3249b90
Gayle Hoefer <gaylehoefer@gmail.com>
2023-10-12 19:37:57 -0700

    add HCDL picture

--- src/Components/Projects/project.jsx
+++ src/Components/Projects/project.jsx
@@ -3,7 +3,7 @@ import { AiFillGithub } from 'react-icons/ai';
 
 export default function Project({projectInfo}) {
     return (
-        <a className="content-card col-11 col-lg-5 my-4" id={projectInfo.id} href={projectInfo.href} target="_blank">
+        <a className="content-card col-11 col-lg-5 my-4" id={projectInfo.id} href={projectInfo.href} target="_blank" rel="noreferrer">
             <div></div>
             <p>{projectInfo.projectInfo}</p>
             <h4>{projectInfo.projectTitle} <AiFillGithub/></h4>
